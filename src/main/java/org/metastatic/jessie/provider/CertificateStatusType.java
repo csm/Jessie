c6373dc194285e5449a33b15100c011975882f17
Casey Marshall <casey.s.marshall@gmail.com>
2014-05-30 10:53:14 -0700

    Importing Jessie from GNU Classpath.

---
+++ src/main/java/org/metastatic/jessie/provider/CertificateStatusType.java
@@ -0,0 +1,13 @@
+package org.metastatic.jessie.provider;
+
+public enum CertificateStatusType
+{
+  OCSP (1);
+
+  public final int value;
+
+  private CertificateStatusType (final int value)
+  {
+    this.value = value;
+  }
+}
